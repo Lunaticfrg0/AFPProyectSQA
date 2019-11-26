f8ae5363a7ce342a604902244c838fcdd7231116
Axcel Espinal <hyrulelink8zelda@gmail.com>
2019-11-26 18:11:29 -0400

    Etapa Final

--- AFPProyectSQA/Views/LogInView.Designer.cs
+++ AFPProyectSQA/Views/LogInView.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInView));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(307, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 36);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "¡Bienvenido!";
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(271, 164);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(116, 58);
+            this.btnEntrar.TabIndex = 8;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Por favor, introduzca sus datos de inicio de sesión:";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(339, 125);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(100, 20);
+            this.txtContrasena.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(254, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Cédula:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(237, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Contraseña:";
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(339, 94);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 20);
+            this.txtCedula.TabIndex = 13;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(397, 164);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(116, 58);
+            this.btnBorrar.TabIndex = 14;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // LogInView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(614, 242);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "LogInView";
             this.Text = "LogInView";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.Button btnBorrar;
     }
 }
--- AFPProyectSQA/Views/LogInView.cs
+++ AFPProyectSQA/Views/LogInView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,79 @@ namespace AFPProyectSQA.Views
         public LogInView()
         {
             InitializeComponent();
+            txtContrasena.UseSystemPasswordChar = true;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtCedula.Clear();
+            txtContrasena.Clear();
+        }
+
+        public string ClientID { get { return this.txtCedula.Text; } }
+
+        public string Contrasena { get { return this.txtContrasena.Text; } }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCedula.Text) == true)
+            {
+                MessageBox.Show("Favor llenar todos los campos.", "AFPServices", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtContrasena.Text) == true)
+            {
+                MessageBox.Show("Favor llenar todos los campos.", "AFPServices", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            String cs = @"Server=localhost;Data Source=LAPTOP-RS890769\SQLEXPRESS;Database=AFPSQAProject;Integrated Security=SSPI";
+
+         
+
+            try
+            {
+                SqlConnection myConnection = default(SqlConnection);
+                myConnection = new SqlConnection(cs);            
+                                                           
+                SqlCommand MyCommand = default(SqlCommand);
+
+                MyCommand = new SqlCommand ("SELECT ([ClientID], [Contrasena]) FROM [dbo].[tblClient] WHERE ClientID = @ClientID AND Contrasena = @Contrasena", myConnection);
+
+
+                SqlParameter paramContrasena = new SqlParameter("@Contrasena", SqlDbType.NVarChar);
+                SqlParameter paramClientID = new SqlParameter("@ClientID", SqlDbType.Int);
+
+                paramContrasena.Value = txtContrasena.Text;
+                paramClientID.Value = txtCedula.Text;
+
+                MyCommand.Connection.Open();
+
+                SqlDataReader MyReader = MyCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+                if (MyReader.Read() == true)
+                {
+                    MainMenuView form2 = new MainMenuView();
+                    form2.Show();
+                    this.Hide();
+                }
+
+                else
+                {
+                    MessageBox.Show("Datos incorrectos.", "AFPServices", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtCedula.Clear();
+                    txtContrasena.Clear();
+                  
+                }
+
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show(EX.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            txtContrasena.Clear();
+            txtCedula.Clear();
         }
     }
 }
